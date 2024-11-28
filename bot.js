4f824671525076df382ffdc33bbcf390dac5dfd1
kambar <mirmanoov@gmail.com>
2024-11-29 01:48:16 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,22 +4,11 @@ const TelegramBot = require('node-telegram-bot-api');
 // Load token from environment variable
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
-if (!token) {
-  console.error('TELEGRAM_BOT_TOKEN is missing. Check your .env file.');
-  process.exit(1); // Exit if the token is not set
-}
-
 const bot = new TelegramBot(token, { polling: true });
 
-// Handle errors
-bot.on('polling_error', (error) => {
-  console.error('Polling error:', error.code, error.message);
-});
-
-// Handle messages
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const webAppUrl = 'https://snake-game-eta-jet.vercel.app/';
+  const webAppUrl = 'https://snake-game-nine-nu.vercel.app/';
 
   // Inline Keyboard Button
   const inlineKeyboard = {
